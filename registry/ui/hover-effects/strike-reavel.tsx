6b7c0674a9db2718433ec2948c6a5edd09d6756b
rahul <r.prahulparihar70@gmail.com>
2026-04-13 14:10:01 +0530

    strike-reveal component added

---
+++ registry/ui/hover-effects/strike-reavel.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+interface StrikeRevealProps {
+  label: string;
+  href?: string;
+}
+
+const StrikeReveal: React.FC<StrikeRevealProps> = ({ label, href = "#" }) => {
+  return (
+    <a
+      href={href}
+      data-letters={label}
+      className="
+        link--kukuri
+        relative 
+        text-[clamp(3rem,5vw,7rem)] 
+        font-black 
+        uppercase 
+        no-underline 
+        text-[#a8a39a] 
+        overflow-hidden
+        
+        /* FIXED LINE (::after) */
+        after:content-['']
+        after:absolute 
+        after:h-[0.8vw] 
+        after:w-full 
+        after:top-1/2 
+        after:left-0 
+        after:-translate-x-full 
+        after:-translate-y-1/2 
+        after:bg-white 
+        after:transition-transform 
+        after:duration-[400ms] 
+        after:ease-[cubic-bezier(0.7,0,0.3,1)]
+        hover:after:translate-x-full
+
+        /* TEXT REVEAL (::before) */
+        before:content-[attr(data-letters)] 
+        before:absolute 
+        before:text-[#2a2a2a] 
+        before:whitespace-nowrap 
+        before:w-0 
+        before:overflow-hidden 
+        before:transition-[width] 
+        before:duration-400 
+        before:delay-[300ms]
+        hover:before:w-full
+      "
+    >
+      {label}
+    </a>
+  );
+};
+
+export default StrikeReveal;
